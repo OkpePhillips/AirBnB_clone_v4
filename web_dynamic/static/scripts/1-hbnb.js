fae5d33d56e0da8a07f7b46cdc3887899de917e1
Godwin Okpe <okpegodwin18@yahoo.com>
2023-11-02 12:09:04 -0700

    Attempting task two: manipulating list tags of amenity div

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Manipulating amenity tags using jquery
+$(document).ready(function () {
+    let selectedAmenities = [];
+    $('.amenity-checkbox').change(function () {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        if (this.checked) {
+            selectedAmenities.push(amenityId);
+        } else {
+            let index = selectedAmenities.indexOf(amenityId);
+            if (index !== -1) {
+                selectedAmenities.splice(index, 1);
+            }
+        }
+        $('.amenities h4').text(selectedAmenities.join(', '));
+    });
+});
