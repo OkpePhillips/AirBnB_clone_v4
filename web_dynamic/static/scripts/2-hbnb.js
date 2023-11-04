5bbb1f4166b89ee8a2a1ab37d2ac15dbec560417
Godwin Okpe <okpegodwin18@yahoo.com>
2023-11-04 01:50:55 -0700

    Adding the scripts for api status task

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// Script to check api status and add it to page
+$(document).ready(function () {
+    let selectedAmenities = [];
+    $('.amenity-checkbox').change(function () {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        if (this.checked) {
+            selectedAmenities.push(amenityName);
+        } else {
+            let index = selectedAmenities.indexOf(amenityName);
+            if (index !== -1) {
+                selectedAmenities.splice(index, 1);
+            }
+        }
+        $('.amenities h4').text(selectedAmenities.join(', '));
+    });
+    function checkApiStatus() {
+        $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+            if (data.status === 'OK') {
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').removeClass('available');
+            }
+        });
+    }
+    checkApiStatus();
+    setInterval(checkApiStatus, 5000);
+});
