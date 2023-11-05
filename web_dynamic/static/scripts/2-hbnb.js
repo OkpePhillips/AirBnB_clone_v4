6429403976d076db5ed069f1b870f7ac19a88916
Obi <williamobi818@gmail.com>
2023-11-05 18:30:39 +0000

    added answers to task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,28 +1,32 @@
 $(document).ready(function () {
-    let selectedAmenities = [];
-    $('.amenity-checkbox').change(function () {
-        const amenityId = $(this).data('id');
-        const amenityName = $(this).data('name');
+  let amenityIds = {}; // This will store the Amenity IDs
 
-        if (this.checked) {
-            selectedAmenities.push(amenityName);
-        } else {
-            let index = selectedAmenities.indexOf(amenityName);
-            if (index !== -1) {
-                selectedAmenities.splice(index, 1);
-            }
-        }
-        $('.amenities h4').text(selectedAmenities.join(', '));
-    });
-    function checkApiStatus() {
-        $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-            if (data.status === 'OK') {
-                $('#api_status').addClass('available');
-            } else {
-                $('#api_status').removeClass('available');
-            }
-        });
+  $('input[type="checkbox"]').on("change", function () {
+    let amenityId = $(this).data("id");
+    let amenityName = $(this).data("name");
+
+    if ($(this).is(":checked")) {
+      // If the checkbox is checked, add the Amenity ID to the dictionary
+      amenityIds[amenityId] = amenityName;
+    } else {
+      // If the checkbox is unchecked, remove the Amenity ID from the dictionary
+      delete amenityIds[amenityId];
     }
-    checkApiStatus();
-    setInterval(checkApiStatus, 5000);
+
+    // Update the h4 tag with the list of checked amenities
+    let amenitiesList = Object.values(amenityIds).join(", ");
+    $(".amenities h4").text(amenitiesList);
+  });
+  function checkApiStatus() {
+    $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+      if (data.status === "OK") {
+        $("#api_status").addClass("available");
+      } else {
+        $("#api_status").removeClass("available");
+      }
+    });
+  }
+  checkApiStatus();
+  setInterval(checkApiStatus, 5000);
 });
+
